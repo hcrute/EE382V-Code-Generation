246ba7718026ce5b784902cd9fd5fe516ddf139a
Henry Crute <henrycrute@gmail.com>
2017-02-24 14:15:37 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # EE382V-Code-Gen-Optimization
 
 for llvm makefiles use CXX=clang
+for compiling down bitcode do clang hello.c -c -emit-llvm -O0 -g
+for running a llvm project on it do opt -load ./prog.so -disable-output -printIR hello.bc
 
---
+++ prog2/epp_runtime.cpp
@@ -0,0 +1,28 @@
+//Henry Crute
+//EE382V
+
+#include "epp_runtime.h"
+
+using namespace std
+
+//initialize 
+void init_path_reg(int loopId) {
+	return;
+}
+
+//increment
+void inc_path_reg(int loopId, int val) {
+	return;
+}
+
+//???
+void finalize_path_reg(int loopId) {
+	return;
+}
+
+//dump current data structure?
+void dump_path_regs() {
+	cout << "Hello World\n";
+	return;
+}
+
